c25a691faee9cd5404a66f7ec86dac641d2e6846
BORNEO-TECH <borneotech177@gmail.com>
2026-04-27 06:05:02 +0700

    Create supabase.js

---
+++ backend/supabase.js
@@ -0,0 +1,15 @@
+const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY,
+  {
+    auth: {
+      autoRefreshToken: true,
+      persistSession: true
+    }
+  }
+);
+
+module.exports = supabase;
