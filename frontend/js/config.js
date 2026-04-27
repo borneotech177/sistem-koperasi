a367328e4e65aa3d79f3809105fd724e25e8df37
BORNEO-TECH <borneotech177@gmail.com>
2026-04-28 05:12:10 +0700

    Create config.js

---
+++ frontend/js/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  API: 'https://sistem-koperasi-production.up.railway.app',
+  APP_NAME: 'SisKop',
+  APP_DESC: 'Sistem Manajemen Koperasi',
+  VERSION: '1.0.0',
+  COMPANY: 'BORNEO-TECH',
+  YEAR: '2026'
+}
